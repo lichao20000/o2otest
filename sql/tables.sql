979f13ffefc72983a03b2b3b3a48c622d8348b80
WY <wy@wy-mac.local>
2018-04-20 15:45:25 +0800

    table channel , sales_depart and init data sql

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,3 +1,109 @@
+create table t_sales_channel(
+    channel_id integer primary key , -- 渠道ID
+    channel_name varchar(100) ,-- 渠道名称
+    last_update_time timestamp default current_timestamp
+);
+
+create  sequence seq_t_sales_channel;
+--  初始化
+
+insert into t_sales_channel( channel_id, channel_name)
+values 
+(nextval('seq_t_sales_channel'),'校园'),
+(nextval('seq_t_sales_channel'),'实渠'),
+(nextval('seq_t_sales_channel'),'营业');
+
+
+create table t_sales_depart(
+    sales_depart_id integer primary key , -- 渠道区分id
+    sales_depart_name varchar(100) not null, --- 渠道名称
+    parent_id  integer not null,
+    channel_id integer not null, 
+    last_update_time timestamp default current_timestamp,
+    last_udpate_user_id varchar(60)
+);
+create sequence seq_t_sales_depart;
+
+--  初始化
+
+insert into t_sales_depart
+(sales_depart_id, channel_id,  sales_depart_name, parent_id )
+values 
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='校园'),'市公司校园', 0),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='实渠'),'市公司实渠', 0),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='营业'),'市公司营业', 0)
+;
+-- parent_id 根据上面创建的设置
+insert into t_sales_depart
+(sales_depart_id, channel_id,  sales_depart_name, parent_id )
+values 
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='校园'),'天河区分',1),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='校园'),'越秀区分',1),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='校园'),'白云区分',1),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='校园'),'番禺区分',1),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='校园'),'海珠区分',1),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='校园'),'花都区分',1),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='校园'),'荔湾区分',1),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='校园'),'黄埔区分',1),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='校园'),'增城区分',1),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='校园'),'从化区分',1),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='校园'),'南沙区分',1),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='实渠'),'天河销售',2),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='实渠'),'海珠销售',2),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='实渠'),'白云南销售',2),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='实渠'),'白云北销售',2),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='实渠'),'番禺销售',2),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='实渠'),'越秀销售',2),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='实渠'),'花都销售',2),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='实渠'),'荔湾销售',2),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='实渠'),'黄埔销售',2),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='实渠'),'增城销售',2),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='实渠'),'南沙销售',2),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='实渠'),'从化销售',2),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='营业'),'天河销售',3),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='营业'),'海珠销售',3),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='营业'),'白云南销售',3),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='营业'),'白云北销售',3),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='营业'),'番禺销售',3),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='营业'),'越秀销售',3),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='营业'),'花都销售',3),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='营业'),'荔湾销售',3),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='营业'),'黄埔销售',3),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='营业'),'增城销售',3),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='营业'),'南沙销售',3),
+(nextval('seq_t_sales_depart'),(select channel_id from t_sales_channel where channel_name ='营业'),'从化销售',3)
+;
+
+
+create view v_sales_depart as 
+select  d.*,
+    (select array_agg(sales_depart_id)||d.sales_depart_id 
+        from t_sales_depart sd 
+    where sd.parent_id = d.sales_depart_id)  as charge_departs
+from t_sales_depart d;
+
+
+
+
+--- 用户表
+-- drop table t_sales_user;
+create table t_sales_user(
+    user_id varchar(60) primary key , -- uni_email
+    user_name varchar(20),
+    mobile varchar(11),
+    channel_id integer , 
+    sales_depart_id integer, 
+    privs varchar[],
+    last_sync_time timestamp default current_timestamp, --基本信息 同步时间
+    last_login_time timestamp default current_timestamp,
+    last_update_time timestamp, --- chanenl depart privs 更新时间
+    last_update_user_id varchar(60) -- 同上
+);
+
+
+
+
+
 -- 促销点
 create table t_sales_pos{ 
     pos_id integer primary key, -- 促销点 ID 本地
@@ -5,6 +111,8 @@ create table t_sales_pos{
     sale_id  varchar(30), -- 促销点ID  导入
     pos_name varchar(100),
     pos_address  text,
-    pos_depart varchar(20),
-    
+    pos_depart varchar(20)
 }
+
+
+
